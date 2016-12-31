3d03e1069d71b5e75b498a0e9b1cc4d751ff1050
Feng Ma <mawisefeng@gmail.com>
2016-12-31 14:02:45 -0500

    slidedwon container

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 require("./styles/styles.scss");
 require("../dist/index.html");
+var jQuery = require("jquery");
 
-var dd = {
+/*var dd = {
 	title : "app from webpack"
 };
 
-var { title } = dd;
-console.log(title);
+var { title } = dd;*/
+
+(function($){
+
+   $(".container > .header").click(function(){
+   	  var _self = this;
+   	  $(_self).siblings(".box").toggleClass("collapsed");
+   	  $(_self).toggleClass("collapsed");
+   });
+
+})(jQuery);
