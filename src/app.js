4e621c4d2f686d51c5a4359c228371ffc529ede0
Feng Ma <mawisefeng@gmail.com>
2017-01-10 22:41:28 -0500

    dropdown list

--- src/app.js
+++ src/app.js
@@ -8,6 +8,80 @@ var jQuery = require("jquery");
 
 var { title } = dd;*/
 
+
+function initializeGallerySelection($){
+
+	var getInsideDropdown = false;
+
+
+	$('#filter_span').click(function(){
+		var htmlNode = $(this).html();
+		
+		
+		$('#retrieveCover_list').children('span').each(function(){
+			if($(this).html() == htmlNode)  $(this).addClass('active');
+	});
+	
+	
+	$('#retrieveCover_list').css({'display':'block'});
+			
+		$(document).bind('mouseup', function(){
+			if(!getInsideDropdown) { 		   
+								$('#retrieveCover_list').animate({height:0},function() {
+							         $(this).css({display:'none','height':'243px'});
+							         $(document).unbind("mouseup");
+						        });
+			 }
+		 });
+		
+		//openDrop();
+		
+	});
+		
+		
+		
+		
+	$('#retrieveCover_list').find('span').on('click',function(){
+	   var htmlContent = $(this).html();
+		
+	   var themeValue = htmlContent;
+	   if(themeValue==GalleryCoversFetcher.DEFAULT_NAME_FOR_ALL_COVERS)themeValue=null;			   
+
+	   var trackValue = themeValue;
+	   if(!trackValue)
+	   		trackValue=GalleryCoversFetcher.DEFAULT_NAME_FOR_ALL_COVERS;
+	   
+	   MMASOmniture.trackAction(trackValue);
+	   filterGalleryByTheme(themeValue);
+	   
+	  //let's not do this until we havea  successful change
+	  // $('#filter_span').html(htmlContent);
+	   var pattern = /Cover/i;
+	   newindex = htmlContent.replace(pattern,"");
+	   updateCover(newindex);
+	   $('#retrieveCover_list').animate({height:0},function() {
+						 $(this).css({display:'none','height':'243px'});
+						 $(document).unbind("mouseup");
+		});
+	   
+	   
+	   
+	   
+	}).on('mouseover',function(){
+	    $('#retrieveCover_list').children('span').each(function(){
+			if($(this).hasClass('active'))  $(this).removeClass('active');
+		});
+	    getInsideDropdown = true;
+	}).on('mouseout',function(){
+		getInsideDropdown = false;
+		//alert(getInsideWindow);
+	});
+	
+}
+
+
+
+
 (function($){
 
    $(".container > .header").click(function(){
@@ -15,5 +89,7 @@ var { title } = dd;*/
    	  $(_self).siblings(".box").toggleClass("collapsed");
    	  $(_self).toggleClass("collapsed");
    });
+
+   initializeGallerySelection($);
 
 })(jQuery);
